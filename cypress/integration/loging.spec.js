e44abe09599e04f6d77ff669e8a6ff8b8d42fb6d
Ekaterina Samuseva <ekaterina.samuseva@fivejars.com>
2021-06-17 12:19:57 +0300

    Log in fix

--- cypress/integration/loging.spec.js
+++ cypress/integration/loging.spec.js
@@ -9,19 +9,19 @@ describe('Smoke testing', () => {
 
       // Log in as user with active membership
       cy.visit(data.home_page)
-      cy.get(homePage.elements.buttons.loginButton).click()
-      cy.get(tnewLoginPage.elements.inputs.emailField).type(data.active_user)
-      cy.get(tnewLoginPage.elements.inputs.passwordField).type(data.active_user_password)
-      cy.get(tnewLoginPage.elements.buttons.singIn).click()
+      cy.get(homePage.elements.buttons.loginButton).click();
+      cy.get(tnewLoginPage.elements.inputs.emailField).type(data.active_user);
+      cy.get(tnewLoginPage.elements.inputs.passwordField).type(data.active_user_password);
+      cy.get(tnewLoginPage.elements.buttons.singIn).click();
 
-      // We should be redirected to https://www.mountvernon.org/ page
-      cy.get(homePage.elements.blocks.userName).should('contain', 'Ekateri')
-      cy.window().its(homePage.elements.blocks.userStatus).should('equal', 'member_active')
+      // Check user's role and name
+      cy.get(homePage.elements.blocks.userName).should('contain', 'Ekateri');
+      cy.window().its(homePage.elements.blocks.userStatus).should('equal', 'member_active');
 
       // Log out
-      cy.get(homePage.elements.blocks.userName).click()
-      cy.get('div.item > a.link').contains('Logout').click()
-      cy.url().should('include', '/account/logout')
+      cy.get(homePage.elements.blocks.userName).click();
+      cy.get('div.item > a.link').contains('Logout').click();
+      cy.url().should('include', '/account/logout');
     })
   })
 
@@ -30,16 +30,17 @@ describe('Smoke testing', () => {
 
       // Log in as user with expired membership
       cy.visit(data.login_page)
-      cy.get(tnewLoginPage.elements.inputs.emailField).type(data.expired_user)
-      cy.get(tnewLoginPage.elements.inputs.passwordField).type(data.expired_user_password)
-      cy.get(tnewLoginPage.elements.buttons.singIn).click()
+      cy.get(tnewLoginPage.elements.inputs.emailField).type(data.expired_user);
+      cy.get(tnewLoginPage.elements.inputs.passwordField).type(data.expired_user_password);
+      cy.get(tnewLoginPage.elements.buttons.singIn).click();
 
       // Check User Role
-      cy.window().its(homePage.elements.blocks.userStatus).should('equal', 'non_member')
+      cy.window().its(homePage.elements.blocks.userStatus).should('equal', 'non_member');
+
       // Log out
-      cy.get(homePage.elements.blocks.userName).click()
-      cy.get('div.item > a.link').contains('Logout').click()
-      cy.url().should('include', '/account/logout')
+      cy.get(homePage.elements.blocks.userName).click();
+      cy.get('div.item > a.link').contains('Logout').click();
+      cy.url().should('include', '/account/logout');
     })
   })
 
@@ -48,16 +49,17 @@ describe('Smoke testing', () => {
 
       // Log in as user with expired membership
       cy.visit(data.login_page)
-      cy.get(tnewLoginPage.elements.inputs.emailField).type(data.nonmember_user)
-      cy.get(tnewLoginPage.elements.inputs.passwordField).type(data.nonmember_user_password)
-      cy.get(tnewLoginPage.elements.buttons.singIn).click()
+      cy.get(tnewLoginPage.elements.inputs.emailField).type(data.nonmember_user);
+      cy.get(tnewLoginPage.elements.inputs.passwordField).type(data.nonmember_user_password);
+      cy.get(tnewLoginPage.elements.buttons.singIn).click();
 
       // Check User Role
-      cy.window().its(homePage.elements.blocks.userStatus).should('equal', 'non_member')
+      cy.window().its(homePage.elements.blocks.userStatus).should('equal', 'non_member');
+
       // Log out
-      cy.get(homePage.elements.blocks.userName).click()
-      cy.get('div.item > a.link').contains('Logout').click()
-      cy.url().should('include', '/account/logout')
+      cy.get(homePage.elements.blocks.userName).click();
+      cy.get('div.item > a.link').contains('Logout').click();
+      cy.url().should('include', '/account/logout');
     })
   })
 })
